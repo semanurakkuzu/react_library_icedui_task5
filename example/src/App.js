f28c57b00a787c48f7721f871fed4694fd147e75
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-16 20:37:23 +0300

    added button types

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { ExampleComponent } from 'icedui'
+import { Button } from 'icedui'
 import 'icedui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button text='Primary button' type='primary' />
+      <Button text='Default button' />
+      <Button text='Dashed button' type='dashed' />
+      <Button text='Text button' type='text' />
+      <Button text='Link button' type='link' />
+    </>
+  )
 }
 
 export default App
